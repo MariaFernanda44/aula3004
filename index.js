3e722286debc57c74c4d17563273bfd1a83268c8
MariaFernanda44 <mclarodossantos432@gmail.com>
2025-05-07 17:49:54 +0000

    aula07/05

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ app.use(express.static('public'));
 
 app.get('/', (request, response) => {
     //response.send('Testando!!!');
-    response.render('home');
-}); 
+    response.render('home' , {nome : "Maria"});
+});
+
+
+app.get('/tabuada', (request, response) => {
+let resultado = [];
+for(let cont = 1;cont <= 10;cont++){
+resultado.push(cont * 5);
+}
+response.render('tabuada', {valores : resultado});
+});
 
 app.listen(3000, () => {
     console.log('Servidor na porta 3000');
